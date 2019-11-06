d9e403612647e05b84b591b8dd4dca6e9fb522a3
FancifulEight <FancifulEight@users.noreply.github.com>
2019-11-05 18:53:37 -0700

    Moved to fix issues

---
+++ Assets/Scripts/Encounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encounter : MonoBehaviour {
+    public enum ActionType {
+        LMB, RMB
+    };
+
+    public ActionType actionToPerform = ActionType.LMB;
+    public List<Human> actors = new List<Human>();
+
+    void Start() {
+        foreach (var item in actors)
+        {
+            item.target = this.transform;
+        }
+    }
+
+    void Update() {
+        if (transform.position.y < -12)
+            Destroy(gameObject);
+    }
+
+    public bool isCorrectAction(bool isLeft) {
+        return (actionToPerform == ActionType.LMB && isLeft) || (actionToPerform == ActionType.RMB && !isLeft);
+    }
+
+    public bool InputGetMouse() {
+        int mouse = (actionToPerform == ActionType.LMB) ? 0:1;
+        return Input.GetMouseButtonDown(mouse) || Input.GetMouseButton(mouse);
+    }
+
+    public void Recruit() {
+        foreach (var item in actors)
+        {
+            item.Recruit();
+            item.transform.parent = null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void Kill() {
+        foreach (var item in actors)
+        {
+            item.Die();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager gm = null;
+    [Header("Audio")]
+    public AudioSource mainSrc;
+    public AudioClip beat, song;
+
+    public float songLengthInSeconds = 48;
+    public float beatTempo = 120;
+    [Range(1, 8)]
+    public int ticksPerBeat = 2;
+    public int totalBeats;
+
+    private double startDSP, tickLength, nextTickTime;
+    private int tickCount = 0;
+    public UnityEvent ticked = new UnityEvent();
+
+    [Header("UI")]
+    public Text readyText;
+    public GameObject[] armyPaths = new GameObject[4];
+    public King king;
+
+    [Header("Gameplay")]
+    public Color armyColour = Color.blue;
+    private float startTime = 0;
+    public Vector2[] armyGoals = new Vector2[4];
+    public int armySplit = 1;
+
+    public List<Human> army = new List<Human>();
+    public List<Transform> allEntities = new List<Transform>();
+
+    public Encounter firstEncounter;
+    public Encounter[] encountersPerBeat = new Encounter[0];
+
+    void OnValidate() {
+        totalBeats = (int)(beatTempo * songLengthInSeconds / 60);
+        System.Array.Resize(ref encountersPerBeat, totalBeats);
+    }
+
+    void Awake() {
+        if (gm == null) {
+            gm = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        beatTempo /= 60;
+
+        tickLength = 1f / (beatTempo * ticksPerBeat);
+        nextTickTime = AudioSettings.dspTime + tickLength;
+
+        //ticked.AddListener(Tick);
+        ticked.AddListener(BounceKing);
+        
+        mainSrc = GetComponent<AudioSource>();
+        mainSrc.clip = beat;
+        mainSrc.Play(); 
+    }
+
+    public void Tick() {
+        //Check if on beat, correct button is pressed
+        if (encountersPerBeat.Length <= tickCount) {
+            Debug.Log(string.Format("Current encountersPerBeat Out of Range Exception Count = {0}", tickCount));
+            return;
+        }
+        if (encountersPerBeat[tickCount] != null) {
+            //Check for hit
+            if (encountersPerBeat[tickCount].InputGetMouse()) {
+                //We Got A Hit!
+                ButtonHit();
+            } else {
+                //We got a Miss...
+                ButtonMissed();
+            }
+        }
+
+        //Check to spawn encounters early
+        int indexToCheck = tickCount + 2;
+        if (encountersPerBeat.Length <= indexToCheck) {
+            Debug.Log(string.Format("Current encountersPerBeat Out of Range Exception Count = {0}", indexToCheck));
+            return;
+        }
+        if (encountersPerBeat[indexToCheck] != null) {
+            //Spawn it 
+        }
+    }
+
+    private int kingBobCount = 0;
+    public int beatsPerBob = 8;
+    public void BounceKing() {
+        //Debug.Log("Count: " + kingBobCount);
+
+        if (kingBobCount < beatsPerBob / 2)
+            king.kingAnim.SetBool("Bob", true);
+        else
+            king.kingAnim.SetBool("Bob", false);
+        
+        kingBobCount = (kingBobCount + 1) % beatsPerBob;
+    }
+
+    public void Conquer() {
+        if (armySplit == 1) return;
+        armySplit = 1;
+
+        for (int i = 0;i < armyPaths.Length;i++) {
+            armyPaths[i].SetActive(i == armySplit - 1);
+        }
+
+        ReorganizeArmy();
+    }
+
+    public void Divide() {
+        if (armySplit == armyGoals.Length) return;
+        armySplit++;
+
+        armyPaths[armySplit - 1].SetActive(true);
+        
+        ReorganizeArmy();
+    }
+
+    public void ReorganizeArmy() {
+        for(int i = 0;i < armySplit;i++) {
+            for (int j = i * army.Count / armySplit;j < (i + 1) * army.Count / armySplit && j < army.Count;j++) {
+                army[j].armyNum = i;
+            }
+        }
+    }
+
+    private bool startingUp = false, musicPlaying = false;
+    // Update is called once per frame
+    void Update() {
+        //King Semaphore
+        king.ChangeFlags(Input.GetMouseButton(0), Input.GetMouseButton(1));
+
+        //Ticks
+        startDSP = AudioSettings.dspTime;
+        startDSP += Time.deltaTime;
+
+        while(startDSP > nextTickTime) {
+            ticked.Invoke();
+            nextTickTime += tickLength;
+            tickCount++;
+        }
+        //End Ticks
+
+        bool left = Input.GetMouseButtonDown(0);
+        bool right = Input.GetMouseButtonDown(1);
+        if ((left || right) && !musicPlaying && !startingUp) {
+            if (firstEncounter.isCorrectAction(left)) {
+                firstEncounter.Recruit();
+            } else {
+                firstEncounter.Kill();
+            }
+            mainSrc.loop = false;
+            startingUp = true;
+            tickCount = 0;
+
+            readyText.text = "READY";
+
+        }
+
+        if (!mainSrc.isPlaying) {
+            if (startingUp) {
+                startingUp = false;
+                readyText.text = "";
+
+                StartMusic();
+            } else {
+                mainSrc.loop = false;
+                mainSrc.clip = beat;
+            }
+            mainSrc.Play();
+
+        } else if (musicPlaying) {
+            UpdateInGame();
+        }
+    }
+
+    public void UpdateInGame() {
+        if (Input.GetMouseButtonDown(0))
+            Conquer();
+        if (Input.GetMouseButtonDown(1))
+            Divide();
+
+        //My Army
+        foreach (Transform t in allEntities) {
+            t.Translate(0, -2 * Time.deltaTime, 0);
+        }
+    }
+
+    public void StartMusic() {
+        musicPlaying = true;
+        mainSrc.loop = false;
+        mainSrc.clip = song;
+
+        startTime = Time.time;
+    }
+
+    public void ButtonHit() {
+        Debug.Log("Hit On Time");
+    }
+
+    public void ButtonMissed() {
+        Debug.Log("Missed Note");
+    }
+}
